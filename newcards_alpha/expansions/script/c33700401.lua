afd2481493d8613fbb1a90262ffdf18e68850473
Nemo Ma <nemoma@nmonolith.com>
2018-02-09 08:42:09 -0500

    Update new cards

--- newcards_alpha/expansions/script/c33700401.lua
+++ newcards_alpha/expansions/script/c33700401.lua
@@ -1,17 +1,17 @@
 --Tipi, the Blue Little Lady
 --AlphaKretin
 --For Nemoma
-function c33700301.initial_effect(c)
-	c:EnableCounterPermit(0x233)
+function c33700401.initial_effect(c)
+	c:EnableCounterPermit(0x441)
 	--place counters
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(82821760,0))
 	e1:SetCategory(CATEGORY_COUNTER)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCondition(c33700301.ctcon)
-	e1:SetTarget(c33700301.cttg)
-	e1:SetOperation(c33700301.ctop)
+	e1:SetCondition(c33700401.ctcon)
+	e1:SetTarget(c33700401.cttg)
+	e1:SetOperation(c33700401.ctop)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
@@ -19,9 +19,9 @@ function c33700301.initial_effect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_HAND)
-	e2:SetCost(c33700301.spcost)
-	e2:SetTarget(c33700301.sptg)
-	e2:SetOperation(c33700301.spop)
+	e2:SetCost(c33700401.spcost)
+	e2:SetTarget(c33700401.sptg)
+	e2:SetOperation(c33700401.spop)
 	c:RegisterEffect(e2)
 	--to grave
 	local e3=Effect.CreateEffect(c)
@@ -30,8 +30,8 @@ function c33700301.initial_effect(c)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCode(EVENT_PHASE+PHASE_END)
 	e3:SetCountLimit(1)
-	e3:SetTarget(c33700301.tgtg)
-	e3:SetOperation(c33700301.tgop)
+	e3:SetTarget(c33700401.tgtg)
+	e3:SetOperation(c33700401.tgop)
 	c:RegisterEffect(e3)
 	--to hand
 	local e4=Effect.CreateEffect(c)
@@ -39,23 +39,23 @@ function c33700301.initial_effect(c)
 	e4:SetCategory(CATEGORY_TOHAND)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_DESTROY)
-	e4:SetCondition(c33700301.thcon)
-	e4:SetTarget(c33700301.thtg)
-	e4:SetOperation(c33700301.thop)
+	e4:SetCondition(c33700401.thcon)
+	e4:SetTarget(c33700401.thtg)
+	e4:SetOperation(c33700401.thop)
 	c:RegisterEffect(e4)
 end
-function c33700301.ctcon(e,tp,eg,ep,ev,re,r,rp)
+function c33700401.ctcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_ADVANCE)
 end
-function c33700301.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c33700401.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,e:GetHandler(),0,0x233)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,e:GetHandler(),0,0x441)
 end
-function c33700301.ctop(e,tp,eg,ep,ev,re,r,rp)
+function c33700401.ctop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	e:GetHandler():AddCounter(0x233,2)
+	e:GetHandler():AddCounter(0x441,2)
 end
-function c33700301.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c33700401.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,c) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
@@ -63,76 +63,76 @@ function c33700301.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(g:GetCount())
 	Duel.SendtoGrave(g,REASON_COST)
 end
-function c33700301.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c33700401.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,e:GetHandler(),0,0x233)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,e:GetHandler(),0,0x441)
 end
-function c33700301.spop(e,tp,eg,ep,ev,re,r,rp)
+function c33700401.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		e:GetHandler():AddCounter(0x233,e:GetLabel())
+		e:GetHandler():AddCounter(0x441,e:GetLabel())
 	end
 end
-function c33700301.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c33700401.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
 		local sel=0
-		if c33700301.hftg(e,tp,eg,ep,ev,re,r,rp,chk) then sel=sel+1 end
-		if c33700301.dktg(e,tp,eg,ep,ev,re,r,rp,chk) then sel=sel+2 end
+		if c33700401.hftg(e,tp,eg,ep,ev,re,r,rp,chk) then sel=sel+1 end
+		if c33700401.dktg(e,tp,eg,ep,ev,re,r,rp,chk) then sel=sel+2 end
 		e:SetLabel(sel)
 		return true
 	end
 	local sel=e:GetLabel()
 	if sel>2 then
 		local p=1-tp
-		if c:IsCanRemoveCounter(tp,0x233,1,REASON_COST) and c:GetCounter(0x233)>1 and Duel.SelectYesNo(tp,aux.Stringid(33700301,2)) then
-			c:RemoveCounter(tp,0x233,1,REASON_COST)
+		if c:IsCanRemoveCounter(tp,0x441,1,REASON_COST) and c:GetCounter(0x441)>1 and Duel.SelectYesNo(tp,aux.Stringid(33700401,2)) then
+			c:RemoveCounter(tp,0x441,1,REASON_COST)
 			p=tp
 		end
-		sel=Duel.SelectOption(p,aux.Stringid(33700301,0),aux.Stringid(33700301,1))+1
+		sel=Duel.SelectOption(p,aux.Stringid(33700401,0),aux.Stringid(33700401,1))+1
 	end
-	if sel==1 then c33700301.hftg(e,tp,eg,ep,ev,re,r,rp,chk) elseif sel==2 then c33700301.dktg(e,tp,eg,ep,ev,re,r,rp,chk) end
+	if sel==1 then c33700401.hftg(e,tp,eg,ep,ev,re,r,rp,chk) elseif sel==2 then c33700401.dktg(e,tp,eg,ep,ev,re,r,rp,chk) end
 	e:SetLabel(sel)
 end
-function c33700301.tgop(e,tp,eg,ep,ev,re,r,rp)
+function c33700401.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local sel=e:GetLabel()
 	if sel<1 or sel>2 then return end
-	if sel==1 then c33700301.hfop(e,tp,eg,ep,ev,re,r,rp) else c33700301.dkop(e,tp,eg,ep,ev,re,r,rp) end
+	if sel==1 then c33700401.hfop(e,tp,eg,ep,ev,re,r,rp) else c33700401.dkop(e,tp,eg,ep,ev,re,r,rp) end
 end
-function c33700301.hftg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=e:GetHandler():GetCounter(0x233)
+function c33700401.hftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=e:GetHandler():GetCounter(0x441)
 	if chk==0 then return ct>0 and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,1-tp,LOCATION_HAND+LOCATION_ONFIELD,0,ct,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,1-tp,LOCATION_HAND+LOCATION_ONFIELD)
 end
-function c33700301.hfop(e,tp,eg,ep,ev,re,r,rp)
-	local ct=e:GetHandler():GetCounter(0x233)
+function c33700401.hfop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetHandler():GetCounter(0x441)
 	local g=Duel.SelectMatchingCard(1-tp,Card.IsAbleToGrave,1-tp,LOCATION_HAND+LOCATION_ONFIELD,0,ct,ct,nil)
 	Duel.SendtoGrave(g,REASON_RULE)
 end
-function c33700301.dktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=e:GetHandler():GetCounter(0x233)
+function c33700401.dktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=e:GetHandler():GetCounter(0x441)
 	if chk==0 then return ct>0 and Duel.IsPlayerCanDiscardDeck(1-tp,ct) end
 	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,1-tp,ct)
 end
-function c33700301.dkop(e,tp,eg,ep,ev,re,r,rp)
-	local ct=e:GetHandler():GetCounter(0x233)
+function c33700401.dkop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetHandler():GetCounter(0x441)
 	Duel.DiscardDeck(1-tp,ct,REASON_RULE)
 end
-function c33700301.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c33700401.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local ct=c:GetCounter(0x233)
+	local ct=c:GetCounter(0x441)
 	e:SetLabel(ct)
 	return ct>0 and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0 and not c:IsReason(REASON_BATTLE) and re and re:GetOwner()~=c
 end
-function c33700301.thtg(e,tp,eg,ep,ev,re,r,rp)
+function c33700401.thtg(e,tp,eg,ep,ev,re,r,rp)
 	local ct=e:GetLabel()
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_GRAVE,0,ct,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
 end
-function c33700301.thop(e,tp,eg,ep,ev,re,r,rp)
+function c33700401.thop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=e:GetLabel()
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_GRAVE,0,ct,ct,nil)
 	if g:GetCount()>0 then
